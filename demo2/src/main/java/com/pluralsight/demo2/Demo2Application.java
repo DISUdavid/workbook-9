9b0e51197f7b7f2f0a65eb75b24d0f68cc790bea
David Disu <disudavid9@gmail.com>
2025-06-17 13:56:30 -0500

    Properties and ProductDao Commit

--- demo2/src/main/java/com/pluralsight/demo2/Demo2Application.java
+++ demo2/src/main/java/com/pluralsight/demo2/Demo2Application.java
@@ -10,14 +10,16 @@ import java.util.List;
 public class Demo2Application {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext beanBag = SpringApplication.run(Demo2Application.class, args);
-        SimpleProductDao dao = beanBag.getBean(SimpleProductDao.class);
+         ConfigurableApplicationContext beanBag = SpringApplication.run(Demo2Application.class, args);
+//        SimpleProductDao dao = beanBag.getBean(SimpleProductDao.class);
+//
+//
+//        List<Product> productList = dao.getAll();
+//        for (Product product : productList) {
+//            System.out.println(product);
+//        }
+//    }
 
-
-        List<Product> productList = dao.getAll();
-        for (Product product : productList) {
-            System.out.println(product);
-        }
+//    }
     }
-
 }
--- demo2/src/main/java/com/pluralsight/demo2/NorthwindApplication.java
+++ demo2/src/main/java/com/pluralsight/demo2/NorthwindApplication.java
@@ -1,4 +1,30 @@
 package com.pluralsight.demo2;
 
-public class NorthwindApplication {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class NorthwindApplication implements CommandLineRunner {
+
+    ProductDao productDao;
+    @Autowired
+    public NorthwindApplication(ProductDao productDao) {
+        this.productDao = productDao;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        List<Product> productList = productDao.getAll();
+        for (Product product : productList) {
+            System.out.println(product);
+        }
+    }
+
+
 }
